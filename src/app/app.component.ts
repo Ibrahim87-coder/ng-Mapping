7f8f63fa8b0edefc5a56898d1dcdded2be24e324
ibrahim87-coder <I.Zakaria@st.ul.edu.lb>
2021-03-09 15:09:01 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit  {
+  title = ' Google Maps ';
+  data=[];
+  lat = 34.426731;
+  lng = 35.842400;
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+
+    this.http.get('https://morning-depths-49872.herokuapp.com/')
+ .subscribe((apiResponse:[])=>{
+ this.data = apiResponse;
+ })
+
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+  
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBORNtVLPtP_dr9hnNg5d4GzpBaVYQJS8w',
+      libraries: ['places']
+    }),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
